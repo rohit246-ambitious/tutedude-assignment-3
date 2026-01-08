10b4e68664d9bdaf6791d91814c68d6fbdc1058a
Rohit Mhatre <rohitmhatre578@gmail.com>
2026-01-09 01:44:26 +0530

    add validation to both task

--- task1.py
+++ task1.py
@@ -3,9 +3,10 @@ def facto_recursive(n):
         return 1
     return n * facto_recursive(n-1)
 
-num = int(input("Enter a number: "))
-if num >= 0:
-    result = facto_recursive(num)
-    print(f"Factorial of {num} is {result}")
+num = input("Enter a number: ")
+if num.isdigit():
+    if int(num) >= 0:
+        result = facto_recursive(int(num))
+        print(f"Factorial of {num} is {result}")
 else:
-    print("Error: Factorial is not defined for negative numbers.")
+    print("Error: Invalid input. or type a non-negative integer.")
--- task2.py
+++ task2.py
@@ -1,18 +1,22 @@
 import math
 
-num = int(input("Enter a number: "))
+num = input("Enter a number: ")
 
-if num >= 1:
-    sqrt_of_num = math.sqrt(num)
-    print(f"square root of {num} is {sqrt_of_num}")
+if num.isdigit() or num.startswith('-'):
+    num = float(num)
+    if num >= 1:
+        sqrt_of_num = math.sqrt(num)
+        print(f"square root of {num} is {sqrt_of_num}")
+    else:
+        print("Error: Square root is only defined for numbers greater than or equal to 1.")
+
+    if num <= 0:
+        print("Error: Natural logarithm is only defined for numbers greater than 0.")
+    else:
+        natural_log = math.log(num)
+        print(f"Logarithm :{natural_log}")
+
+    sine_of_num = math.sin(num)
+    print(f"Sine of {num} is {sine_of_num}")
 else:
-    print("Error: Square root is only defined for numbers greater than or equal to 1.")
-
-if num <= 0:
-    print("Error: Natural logarithm is only defined for numbers greater than 0.")
-else:
-    natural_log = math.log(num)
-    print(f"Logarithm :{natural_log}")
-
-sine_of_num = math.sin(num)
-print(f"Sine of {num} is {sine_of_num}")
+    print("Error: Invalid input. Please enter a valid number.")
